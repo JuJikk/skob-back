8b3b6616d8d7f3b02b80711c821edcc8d864c452
OrestSonich <sonich.work@gmail.com>
2024-08-24 20:00:29 +0300

    fix(protect endpoint): for Foreman access is denied to a Scout who is not in the group

--- src/modules/users/users.controller.ts
+++ src/modules/users/users.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, NotFoundException, Param, Patch, UnauthorizedException, UsePipes, ValidationPipe } from "@nestjs/common"
+import { Controller, ForbiddenException, Get, NotFoundException, Param, Patch, UsePipes, ValidationPipe } from "@nestjs/common"
 import { UsersService } from "./users.service"
 import { EmailDto } from "../common/dto/email.dto"
 import { User } from "../common/decorators/user.decorator"
@@ -20,12 +20,16 @@ export class UsersController {
     return UserDto.toDto(user)
   }
 
+  @Roles(Role.FOREMAN)
   @UsePipes(new ValidationPipe())
   @Get(":email")
   async getUserByEmail(@Param() params: EmailDto, @User() { email: userEmail }: JwtPayloadDto) {
     const user = await this.userService.getUserByEmail(params.email)
     if (!user) {
       throw new NotFoundException(`User not found with email ${params.email} not found.`)
+    }
+    if (user.ownerEmail !== userEmail) {
+      throw new ForbiddenException(`access is denied to a Scout who is not in the your group`)
     }
     return UserDto.toDto(user)
   }
