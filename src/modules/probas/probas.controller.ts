d7740eea966b60d53b4ba6ea74d435d533e630c6
OrestSonich <sonich.work@gmail.com>
2024-08-25 20:14:34 +0300

    feat(probas): add possibility to change enitre proba values, add proba value validation 0 or 1

--- src/modules/probas/probas.controller.ts
+++ src/modules/probas/probas.controller.ts
@@ -6,6 +6,7 @@ import { Role } from "../common/enums/role.enum"
 import { Roles } from "../common/decorators/roles.decorator"
 import { User } from "../common/decorators/user.decorator"
 import { JwtPayloadDto } from "../auth/dto/jwtpayload.dto"
+import { UpdateEntireProbasDto } from "./dto/update.entire.probas.dto";
 
 @Controller("probas")
 export class ProbasController {
@@ -16,5 +17,12 @@ export class ProbasController {
   @UsePipes(new ValidationPipe())
   async updateProba(@Param() params: EmailDto, @User() { email: foremanEmail }: JwtPayloadDto, @Body() proba: UpdateProbasDto) {
     return await this.probasService.updateProba(params.email, foremanEmail, proba)
+  }
+
+  @Patch("/entire/:email")
+  @Roles(Role.FOREMAN)
+  @UsePipes(new ValidationPipe())
+  async updateEntireProba(@Param() params: EmailDto, @User() { email: foremanEmail }: JwtPayloadDto, @Body() proba: UpdateEntireProbasDto) {
+    return await this.probasService.updateEntireProba(params.email, foremanEmail, proba)
   }
 }
